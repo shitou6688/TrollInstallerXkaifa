c1c3c67158283914bdd89171b199d5d15d36474e
shitou6688 <shitou6688@users.noreply.github.com>
2026-08-02 00:44:54 +0800

    install log height: /1.75 -> /1.7, slightly taller

--- TrollInstallerX/UI/MainView.swift
+++ TrollInstallerX/UI/MainView.swift
@@ -447,14 +447,14 @@ struct MainView: View {
                             RoundedRectangle(cornerRadius: 16, style: .continuous)
                                 .fill(Color.white.opacity(0.05))
                                 .frame(maxWidth: geometry.size.width / 1.2)
-                                .frame(maxHeight: isInstalling ? geometry.size.height / 1.75 : 60)
+                                .frame(maxHeight: isInstalling ? geometry.size.height / 1.7 : 60)
                                 .transition(.scale)
                                 .shadow(radius: 10)
                             if isInstalling {
                                 LogView(installationFinished: $installationFinished)
                                     .padding()
                                     .frame(maxWidth: geometry.size.width / 1.2)
-                                    .frame(maxHeight: geometry.size.height / 1.75)
+                                    .frame(maxHeight: geometry.size.height / 1.7)
                             } else {
                                 Button(action: {
                                     if !isShowingCredits && !isShowingSettings && !isShowingMDCAlert && !isShowingOTAAlert {
